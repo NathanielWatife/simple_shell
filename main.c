8a5051eb0115dd7314ac22305d42162bd364d8fe
NathanielWatife <awosanminathaniel0@gmail.com>
2023-09-17 10:59:22 -0700

    Files generated already

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - start the shell
+ * @argc: Counter of arguments received
+ * @argv: Pointer to arguments received
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	shell_loop(argv);
+	return (0);
+}
