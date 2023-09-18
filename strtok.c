b0db5d7af6067ea0407eafc4a604fddc55ec16b1
theijhay <olawaleisaacjohn@gmail.com>
2023-09-18 14:42:27 -0700

    fixed bugs

---
+++ strtok.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * _strtok - tokenizes the strtok.
+ * @buffer: the string of the strtok.
+ * @delim: the dividing char of strtok.
+ * Return: char * to token if it's successeful, otherwise NULL.
+ */
+
+char *_strtok(char *buffer, const char *delim)
+{
+	static char *sp;
+	char *p;
+	int letter = 0;
+	int i = 0;
+	int stop = 0;
+
+	if (buffer)
+	sp = buffer;
+	p = sp;
+	while (sp && *sp)
+	{
+	while (delim[i])
+	{
+	if (*sp == delim[i] && letter == 0)
+	{
+	++p;
+	++sp;
+	i = 0;
+	continue;
+	}
+	if (*sp == delim[i] && letter == 1)
+	{
+	*sp++ = '\0';
+	stop = 1;
+	i = 0;
+	break;
+	}
+	++i;
+	}
+	if (stop || !*sp)
+	break;
+	letter = 1;
+	i = 0;
+	++sp;
+	}
+	if (!p || *p == '\0')
+	return (NULL);
+	return (p);
+}
