3e368165d89dde6d9d26bae67afc5c4382e13b33
theijhay <olawaleisaacjohn@gmail.com>
2023-09-21 06:40:54 -0700

    Fixing betty

--- shell.h
+++ shell.h
@@ -1,11 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-/* Newly added */
-#ifndef _SHELL_H_
-#define _SHELL_H_
-#include <fcntl.h>
-#include <signal.h>
-/*ends here*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +10,8 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #define FREE(x) (x = (free(x), NULL))
 #define PROMPT " $ "
@@ -74,32 +70,23 @@ char **_get_str_env(env_list_t **env);
 void *op_push_end(order_t **ops, int n);
 char **_get_cmds(char *line, order_t **ops);
 void free_ops(order_t **ops);
+
+/*Functions*/
 extern char **environ;
 extern int *LINE_COUNT;
 extern char **FNC_NAME;
 extern char **environ;
-char *_getline(void);
 char **_splitline(char *line_buf);
-int _execute(char **args, int *flag, int mode);
 char *_pathfinder(char *command, int mode);
 char *_catpath(char **uncatpath, char *command);
-int _builtins(char **args);
-void _cd(char **args);
+int _execute(char **args, int *flag, int mode);
 void _env(void);
 
-/*Utilities*/
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-int _strcmp_path(char *s1, char *s2);
 unsigned int _strcspn(char *s, char *pre);
 int _strspn(char *s, const char *delim);
 void _nonintmode(char *line, char **args);
 char *_strchr(char *s, char b);
-
+int _strcmp_path(char *s1, char *s2);
 /*Proto-Functions*/
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *_strtok_r(char *s, const char *delim, char **save_str);
-char *_strtok(char *s, char *deli);
 #endif
