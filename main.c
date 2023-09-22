aeb08c1d339155f354c56ea0d97897946450f223
theijhay <olawaleisaacjohn@gmail.com>
2023-09-21 18:40:03 -0700

    fixed bugs and command line

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ void shell(vars_t *build)
 {
 	while (true)
 	{
-	checkAndGetLine(build);
-	if (splitString(build) == false)
-	continue;
-	if (findinborns(build) == true)
-	continue;
-	check_path(build);
-	forkAndExecute(build);
+		checkAndGetLine(build);
+		if (splitString(build) == false)
+			continue;
+		if (findinborns(build) == true)
+			continue;
+		checkPath(build);
+		forkAndExecute(build);
 	}
 }
 
@@ -33,21 +33,22 @@ void checkAndGetLine(vars_t *build)
 	build->enviromentList = NULL;
 	build->count++;
 	if (isatty(STDIN_FILENO))
-	displayPrompt();
+		displayPrompt();
 	len = getline(&build->buffer, &bufferSize, stdin);
 	if (len == EOF)
 	{
-	free_members(build);
-	if (isatty(STDIN_FILENO))
-	displayNewLine();
-	if (build->error)
-	exit(build->error);
-	exit(EXIT_SUCCESS);
+		freeMembers(build);
+		if (isatty(STDIN_FILENO))
+			displayNewLine();
+		if (build->error)
+			exit(build->error);
+		exit(EXIT_SUCCESS);
+
 	}
 	ptr = _strchr(build->buffer, '\n');
 	ptr2 = _strchr(build->buffer, '\t');
 	if (ptr || ptr2)
-	insertNullByte(build->buffer, len - 1);
+		insertNullByte(build->buffer, len - 1);
 	stripComments(build->buffer);
 }
 
@@ -63,21 +64,21 @@ void stripComments(char *str)
 
 	while (str[i])
 	{
-	if (i == 0 && str[i] == '#')
-	{
-	insertNullByte(str, i);
-	return;
-	}
-	if (notFirst)
-	{
-	if (str[i] == '#' && str[i - 1] == ' ')
-	{
-	insertNullByte(str, i);
-	return;
-	}
-	}
-	i++;
-	notFirst = true;
+		if (i == 0 && str[i] == '#')
+		{
+			insertNullByte(str, i);
+			return;
+		}
+		if (notFirst)
+		{
+			if (str[i] == '#' && str[i - 1] == ' ')
+			{
+				insertNullByte(str, i);
+				return;
+			}
+		}
+		i++;
+		notFirst = true;
 	}
 }
 
@@ -93,30 +94,30 @@ void forkAndExecute(vars_t *build)
 	convertLLtoArr(build);
 	if (f1 == -1)
 	{
-	perror("error\n");
-	free_members(build);
-	free_args(build->enviromentList);
-	exit(1);
+		perror("error\n");
+		freeMembers(build);
+		freeArgs(build->enviromentList);
+		exit(1);
 	}
 	if (f1 == 0)
 	{
-	if (execve(build->fpath, build->args, build->enviromentList) == -1)
-	{
-	errorHandler(build);
-	free_members(build);
-	free_args(build->enviromentList);
-	if (errno == ENOENT)
-	exit(127);
-	if (errno == EACCES)
-	exit(126);
-	}
+		if (execve(build->fpath, build->args, build->enviromentList) == -1)
+		{
+			errorHandler(build);
+			freeMembers(build);
+			freeArgs(build->enviromentList);
+			if (errno == ENOENT)
+				exit(127);
+			if (errno == EACCES)
+				exit(126);
+		}
 	} else
 	{
-	wait(&status);
-	if (WIFEXITED(status))
-	build->error = WEXITSTATUS(status);
-	free_args_and_buffer(build);
-	free_args(build->enviromentList);
+		wait(&status);
+		if (WIFEXITED(status))
+			build->error = WEXITSTATUS(status);
+		freeArgsAndBuffer(build);
+		freeArgs(build->enviromentList);
 	}
 }
 
@@ -135,14 +136,14 @@ void convertLLtoArr(vars_t *build)
 	enviromentlist = malloc(sizeof(char *) * (count + 1));
 	if (!enviromentlist)
 	{
-	perror("Malloc failed\n");
-	exit(1);
+		perror("Malloc failed\n");
+		exit(1);
 	}
 	while (tmp)
 	{
-	enviromentlist[i] = _strdup(tmp->str);
-	tmp = tmp->nxt;
-	i++;
+		enviromentlist[i] = _strdup(tmp->str);
+		tmp = tmp->nxt;
+		i++;
 	}
 	enviromentlist[i] = NULL;
 	build->enviromentList = enviromentlist;
